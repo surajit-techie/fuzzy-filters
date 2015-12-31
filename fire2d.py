adb8cdf3e4213d4a79a051e54021aa40cd67921f
Surajit Dasgupta <surajit.techie@gmail.com>
2016-01-01 03:55:43 +0530

    Added files - fire2D.py

---
+++ fire2d.py
@@ -0,0 +1,26 @@
+import os
+import cv2
+import random
+import numpy as np
+import skfuzzy as fuzz
+
+def sp_noise(image,prob):
+	output = np.zeros(image.shape,np.uint8)
+	thres = 1 - prob 
+	for i in range(image.shape[0]):
+		for j in range(image.shape[1]):
+			rdn = random.random()
+			if rdn < prob:
+				output[i][j] = 0
+			elif rdn > thres:
+				output[i][j] = 255
+			else:
+				output[i][j] = image[i][j]
+	return output
+
+image = cv2.imread('lena.jpg')
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+noise_image = sp_noise(gray_image,0.05)
+cv2.imwrite('noiseimg.png',noise_image)
+fuzz_image = fuzz.filters.fire2d(noise_image, 0, 260,1)
+cv2.imwrite('fuzzimg.png',fuzz_image)
